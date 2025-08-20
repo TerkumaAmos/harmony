3e48d00e3c0da75e39be308f97caf29dc165db0b
TerkumaAmos <agberterkuma2020@gmail.com>
2025-08-20 10:04:02 +0100

    splash_screen_navigation

--- lib/features/shared/presentation/screens/splash_Screen.dart
+++ lib/features/shared/presentation/screens/splash_Screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:harmony_hush/core/theme/app_colors.dart';
+import 'package:go_router/go_router.dart'; // Import GoRouter
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToOnboard();
+  }
+
+  Future<void> _navigateToOnboard() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (mounted) {
+      context.go('/onboard'); // Use GoRouter to navigate
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Center(child: Image(image: AssetImage('assets/img/Logo.png')),),
+      body: Center(
+        child: Image(image: AssetImage('assets/img/Logo.png')),
+      ),
     );
   }
 }
